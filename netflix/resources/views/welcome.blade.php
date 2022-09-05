a50ad2620f7997e74fbdc2e89a5f3c182610f7e0
gabriela.debora <gabriela.debora@pmc.corp>
2022-09-05 12:49:04 -0300

    novo

---
+++ netflix/resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflix Brasil - assistir a séries online, assistir filmes online</title>
+    <link rel="shortcut icon" href="{{ url('') }}/icon.png">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <body>
+        <main class="main">
+            <div class="main-child">
+                <h1 class="our-story-card-title" data-uia="hero-title">Filmes, séries e muito mais. Sem limites.</h1>
+                <span>Assista onde quiser. Cancele quando quiser.</span>
+                <form class="form-cta">
+                    <input type="text" placeholder="Email">
+                    <button>Vamos lá!</button>
+                </form>
+                <p>
+                    Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.
+                </p>
+            </div>
+            <div class="gradient"></div>
+        </main>
+        <section class="section">
+            <div class="container">
+                <div class="grid">
+                    <div>
+                        <h1>Aproveita na TV.</h1>
+                        <p>
+                            Assista em Smart Tvs. Playstation, Xbox, Chromecast, Apple TV, aparelhos de Blu-rays e outros aparelhos.
+                        </p>
+                    </div>
+                    <div class="img-tv">
+                        <img src="img/tv.png" alt="TV">
+                        <video class="video-tv" autoplay playsinline muted loop>
+                    <source 
+                    src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4">
+                </video>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- Segunda etapa -->
+
+        <section class="download section">
+            <div class="container">
+                <div class="grid">
+                    <div>
+                        <h1>Baixe séries para assistir offline.</h1>
+                        <p>
+                            Salve seus títulos favoritos e sempre tenha algo para assistir. </p>
+                    </div>
+                    <div class="img-tv">
+                        <img src="img/ga.jpg" alt="EI" />
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- Terceira etapa -->
+        <section class="device-pile section">
+            <div class="container">
+                <div class="grid">
+                    <div>
+                        <h1>Assista quando quiser.</h1>
+                        <p>
+                            Assista no celular, tablet, Smart TV ou notebook sem pagar a mais por isso. </p>
+                    </div>
+                    <div class="img-tv">
+                        <img src="img/ba.png" alt="Device Pile">
+                        <video class="video-tv" autoplay playsinline muted loop>
+                <source 
+                src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"  type="video/mp4">
+            </video>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="download section">
+            <div class="container">
+                <div class="grid">
+                    <div>
+                        <h1>Crie perfis para crianças.ss</h1>
+                        <p>
+                            Deixe as crianças se aventurarem com seus personagens favoritos em um espaço feito só para elas, sem pagar a mais por isso. </p>
+                    </div>
+                    <div class="img-tv">
+                        <img src="img/ha.png" alt="EI" />
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <footer class="footer">
+            <form class="form-cta">
+                <input type="text" placeholder="Email">
+                <button>Vamos lá!</button>
+            </form>
+            <p>Copyright <i class="fa-sharp fa-solid fa-copyright"></i> 2022 Netflix Brasil</p>
+        </footer>
+    </body>
+
+</html>
